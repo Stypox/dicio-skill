4a926001c1ab659857709a21554b2972a32a4fb6
Stypox <stypox@pm.me>
2019-07-29 15:26:34 +0200

    Fix nextAssistanceComponents should return a list of components

--- assistance_component/src/main/java/com/dicio/output/OutputGenerator.java
+++ assistance_component/src/main/java/com/dicio/output/OutputGenerator.java
@@ -3,10 +3,11 @@ package com.dicio.output;
 import com.dicio.AssistanceComponent;
 import com.dicio.output.constructs.BaseConstruct;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OutputGenerator {
     BaseConstruct getOutput();
     Optional<OutputGenerator> nextOutputGenerator();
-    Optional<AssistanceComponent> nextAssistanceComponent();
+    Optional<List<AssistanceComponent>> nextAssistanceComponents();
 }
