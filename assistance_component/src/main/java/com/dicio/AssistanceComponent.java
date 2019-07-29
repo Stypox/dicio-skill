48259be12337ba6c353e934261f4f7ed29ea704d
Stypox <stypox@pm.me>
2019-07-29 15:08:44 +0200

    Add AssistanceComponent interface

---
+++ assistance_component/src/main/java/com/dicio/AssistanceComponent.java
@@ -0,0 +1,7 @@
+package com.dicio;
+
+import com.dicio.output.OutputGenerator;
+import com.dicio.input_recognition.InputRecognitionUnit;
+
+public interface AssistanceComponent extends OutputGenerator, InputRecognitionUnit {
+}
