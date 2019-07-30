f2ac19f549d5faf1156c99551a1e9ae3278770c3
Stypox <stypox@pm.me>
2019-07-30 11:44:17 +0200

    Add ViewList needed by OutputGenerator
    It represents a list of views

---
+++ assistance_component/src/main/java/com/dicio/output/views/ViewList.java
@@ -0,0 +1,27 @@
+package com.dicio.output.views;
+
+/**
+ * It does not implement {@link BaseView} to prevent list inception.
+ * <!-- TODO is this ok? -->
+ */
+public class ViewList {
+    private final BaseView[] views;
+
+    /**
+     * Constructs a list of views. A list of views cannot
+     * contain another list of views.
+     * @param views the entries of the list in order
+     */
+    public ViewList(final BaseView... views) {
+        this.views = views;
+    }
+
+    /**
+     * Get the entries of the list in the order
+     * they were provided to the constructor.
+     * @return all list entries
+     */
+    public BaseView[] getViews() {
+        return views;
+    }
+}
