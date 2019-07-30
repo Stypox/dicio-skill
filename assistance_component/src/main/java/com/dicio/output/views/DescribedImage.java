a4df6d96cd6b5d127bf9239c60c36105151bb19e
Stypox <stypox@pm.me>
2019-07-30 11:48:00 +0200

    Add DescribedImage view along with tests
    It is basically the same as Header+Description+Image but they are displayed roughly this way:
    IMAGEIMA     Header
    GEIMAGEI      Description ... ...
    MAGEIMA      ... ... ...

---
+++ assistance_component/src/main/java/com/dicio/output/views/DescribedImage.java
@@ -0,0 +1,85 @@
+package com.dicio.output.views;
+
+import java.util.Optional;
+
+public class DescribedImage implements BaseView {
+    interface OnClickListener {
+        void onClick(String imageSource, Image.SourceType imageSourceType, String headerText, String descriptionText);
+    }
+
+    private final String imageSource;
+    private final Image.SourceType imageSourceType;
+    private final String headerText;
+    private final String descriptionText;
+    private OnClickListener onClickListener;
+
+
+    /**
+     * Constructs a composite view made of an image with header and description
+     *
+     * @see Image.SourceType
+     * @param imageSource a string representing the source of the image
+     * @param imageSourceType the type of the image source
+     * @param headerText text to display in the header, html is not allowed
+     * @param descriptionText text to display in the description, html is allowed
+     */
+    public DescribedImage(String imageSource, Image.SourceType imageSourceType, String headerText, String descriptionText) {
+        this.imageSource = imageSource;
+        this.imageSourceType = imageSourceType;
+        this.headerText = headerText;
+        this.descriptionText = descriptionText;
+        this.onClickListener = null;
+    }
+
+
+    /**
+     * @return a string representing the source of the image to be displayed
+     */
+    public String getImageSource() {
+        return imageSource;
+    }
+
+    /**
+     * @return the type of the image source
+     */
+    public Image.SourceType getImageSourceType() {
+        return imageSourceType;
+    }
+
+    /**
+     * @return plain text to be displayed in the header
+     */
+    public String getHeaderText() {
+        return headerText;
+    }
+
+    /**
+     * @return html text to be displayed in the description
+     */
+    public String getDescriptionText() {
+        return descriptionText;
+    }
+
+
+    /**
+     * Set the click listener for the whole described image.
+     *
+     * @see OnClickListener
+     * @param onClickListener lambda to be called on click
+     */
+    void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    /**
+     * Call the click listener for the whole described image.
+     *
+     * @see Image.OnClickListener
+     * @see #setOnClickListener(OnClickListener)
+     */
+    public void onClick() {
+        if (onClickListener != null) {
+            onClickListener.onClick(imageSource, imageSourceType, headerText, descriptionText);
+        }
+    }
+}
---
+++ assistance_component/src/test/java/com/dicio/output/views/DescribedImageTest.java
@@ -0,0 +1,49 @@
+package com.dicio.output.views;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DescribedImageTest {
+    private int nrClickListenerCalled;
+
+    @Test
+    public void testOnClickListeners() {
+        final String source = "a.b";
+        final Image.SourceType type = Image.SourceType.url;
+        final String header = "header";
+        final String description = "desc";
+
+        DescribedImage di = new DescribedImage(source, type, header, description);
+
+        nrClickListenerCalled = 0;
+        di.setOnClickListener(new DescribedImage.OnClickListener() {
+            @Override
+            public void onClick(String imageSource, Image.SourceType sourceType, String headerText, String descriptionText) {
+                assertEquals(source, imageSource);
+                assertEquals(type, sourceType);
+                assertEquals(header, headerText);
+                assertEquals(description, descriptionText);
+                ++nrClickListenerCalled;
+            }
+        });
+
+        di.onClick();
+        assertEquals(1, nrClickListenerCalled);
+    }
+
+    @Test
+    public void testConstructorAndGetters() {
+        final String source = "a,b";
+        final Image.SourceType type = Image.SourceType.other;
+        final String header = "header";
+        final String description = "desc";
+
+        DescribedImage di = new DescribedImage(source, type, header, description);
+
+        assertEquals(source, di.getImageSource());
+        assertEquals(type, di.getImageSourceType());
+        assertEquals(header, di.getHeaderText());
+        assertEquals(description, di.getDescriptionText());
+    }
+}
