e97e6bb53e2a81c3cedb64eb6d73cbec307fbbf2
Stypox <stypox@pm.me>
2020-06-01 09:17:22 +0200

    Refactor Sentence.java: split methods

--- assistance_component/src/main/java/com/dicio/component/standard/Sentence.java
+++ assistance_component/src/main/java/com/dicio/component/standard/Sentence.java
@@ -9,15 +9,31 @@ public class Sentence {
     private List<String> inputWords;
     private PartialScoreResult[][][] memory;
 
-    //////////////////
-    // Constructors //
-    //////////////////
+    ////////////////////
+    // PUBLIC METHODS //
+    ////////////////////
 
     public Sentence(final String sentenceId, final Word... words) {
         this.sentenceId = sentenceId;
         this.words = words;
     }
 
+
+    public float score(final List<String> inputWords) {
+        this.inputWords = inputWords;
+        memory = new PartialScoreResult[words.length][inputWords.size()][2];
+        return bestScore(0, 0, false).value(words.length, inputWords.size());
+    }
+
+    public StandardResult toStandardResult() {
+        // assume memory has already been calculated
+        return new StandardResult(sentenceId, bestScore(0, 0, false).getCapturingGroups());
+    }
+
+
+    ///////////
+    // SCORE //
+    ///////////
 
     /**
      * Dynamic programming implementation along two dimensions
@@ -40,70 +56,68 @@ public class Sentence {
             return new PartialScoreResult(memory[wordIndex][inputWordIndex][foundWordAfterStartInt]);
         }
 
-        PartialScoreResult result = null;
+        PartialScoreResult result;
         if (words[wordIndex].isCapturingGroup()) {
-            // do not use recursion here, to make checking whether anything was captured simpler
-
-            // first try to skip capturing group
-            result = bestScore(words[wordIndex].getNextIndices()[0],
-                        inputWordIndex, foundWordAfterStart)
-                    .skipCapturingGroup();
-            for (int i = 1; i < words[wordIndex].getNextIndices().length; ++i) {
-                result = bestScore(words[wordIndex].getNextIndices()[i],
-                            inputWordIndex, foundWordAfterStart)
-                        .skipCapturingGroup()
-                        .keepBest(result, words.length, inputWords.size());
-            }
-
-            // then try various increasing lengths of capturing group
-            for (int i = inputWordIndex + 1; i <= inputWords.size(); ++i) {
-                for (int nextIndex : words[wordIndex].getNextIndices()) {
-                    // keepBest will keep the current (i.e. latest) result in case of equality
-                    // so bigger capturing groups are preferred
-                    result = bestScore(nextIndex, i, true)
-                            .setCapturingGroup(words[wordIndex].toString(),
-                                    new InputWordRange(inputWordIndex, i))
-                            .keepBest(result, words.length, inputWords.size());
-                }
-            }
-
+            result = bestScoreCapturingGroup(wordIndex, inputWordIndex, foundWordAfterStart);
         } else {
-            result = bestScore(wordIndex, inputWordIndex + 1, foundWordAfterStart)
-                    .skipInputWord(foundWordAfterStart);
-
-            if (words[wordIndex].toString().equals(inputWords.get(inputWordIndex))) {
-                for (int nextIndex : words[wordIndex].getNextIndices()) {
-                    result = bestScore(nextIndex, inputWordIndex + 1, true)
-                            .keepBest(result, words.length, inputWords.size());
-                }
-                result.foundWordBeforeEnd();
-
-            } else {
-                for (int nextIndex : words[wordIndex].getNextIndices()) {
-                    result = bestScore(nextIndex, inputWordIndex, foundWordAfterStart)
-                            .skipWord()
-                            .keepBest(result, words.length, inputWords.size());
-                }
-            }
+            result = bestScoreNormalWord(wordIndex, inputWordIndex, foundWordAfterStart);
         }
 
         memory[wordIndex][inputWordIndex][foundWordAfterStartInt] = result;
         return new PartialScoreResult(result); // clone object to prevent edits
     }
 
-    float score(final List<String> inputWords) {
-        this.inputWords = inputWords;
-        memory = new PartialScoreResult[words.length][inputWords.size()][2];
-        return bestScore(0, 0, false).value(words.length, inputWords.size());
+    private PartialScoreResult bestScoreCapturingGroup(int wordIndex,
+                                                       int inputWordIndex,
+                                                       boolean foundWordAfterStart) {
+        // do not use recursion here, to make checking whether anything was captured simpler
+        // first try to skip capturing group
+        PartialScoreResult result = bestScore(words[wordIndex].getNextIndices()[0],
+                inputWordIndex, foundWordAfterStart)
+                .skipCapturingGroup();
+        for (int i = 1; i < words[wordIndex].getNextIndices().length; ++i) {
+            result = bestScore(words[wordIndex].getNextIndices()[i],
+                    inputWordIndex, foundWordAfterStart)
+                    .skipCapturingGroup()
+                    .keepBest(result, words.length, inputWords.size());
+        }
+
+        // then try various increasing lengths of capturing group
+        for (int i = inputWordIndex + 1; i <= inputWords.size(); ++i) {
+            for (int nextIndex : words[wordIndex].getNextIndices()) {
+                // keepBest will keep the current (i.e. latest) result in case of equality
+                // so bigger capturing groups are preferred
+                result = bestScore(nextIndex, i, true)
+                        .setCapturingGroup(words[wordIndex].toString(),
+                                new InputWordRange(inputWordIndex, i))
+                        .keepBest(result, words.length, inputWords.size());
+            }
+        }
+
+        return result;
     }
 
+    private PartialScoreResult bestScoreNormalWord(int wordIndex,
+                                                   int inputWordIndex,
+                                                   boolean foundWordAfterStart) {
+        PartialScoreResult result = bestScore(wordIndex, inputWordIndex + 1, foundWordAfterStart)
+                .skipInputWord(foundWordAfterStart);
 
-    //////////
-    // Data //
-    //////////
+        if (words[wordIndex].toString().equals(inputWords.get(inputWordIndex))) {
+            for (int nextIndex : words[wordIndex].getNextIndices()) {
+                result = bestScore(nextIndex, inputWordIndex + 1, true)
+                        .keepBest(result, words.length, inputWords.size());
+            }
+            result.foundWordBeforeEnd();
 
-    StandardResult toStandardResult() {
-        // assume memory has already been calculated
-        return new StandardResult(sentenceId, bestScore(0, 0, false).getCapturingGroups());
+        } else {
+            for (int nextIndex : words[wordIndex].getNextIndices()) {
+                result = bestScore(nextIndex, inputWordIndex, foundWordAfterStart)
+                        .skipWord()
+                        .keepBest(result, words.length, inputWords.size());
+            }
+        }
+
+        return result;
     }
 }
