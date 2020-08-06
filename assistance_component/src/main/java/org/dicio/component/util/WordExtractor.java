fa854d6ca1e97d1fbd7e0a6ad1d9b8f7e464ac90
Stypox <stypox@pm.me>
2020-08-06 13:12:52 +0200

    Add javadoc to util.WordExtractor functions

--- assistance_component/src/main/java/org/dicio/component/util/WordExtractor.java
+++ assistance_component/src/main/java/org/dicio/component/util/WordExtractor.java
@@ -14,13 +14,21 @@ public class WordExtractor {
     private WordExtractor() {
     }
 
+
+    /**
+     * Splits the input into words at every non-letter character, and making every word lowercase.
+     * <br>
+     * For example, "Hello, how Àrè you? " becomes {"hello", "how, "àrè", "you"}
+     * @param input the input from which to extract words
+     * @return the list of extracted words in order
+     */
     public static List<String> extractWords(final String input) {
         // match all non-letter characters
         final String[] splitInput = input.split("[^\\p{L}]+");
 
         final List<String> inputWords = new ArrayList<>();
-        for(String word : splitInput) {
-            String normalized = normalizeWord(word);
+        for(final String word : splitInput) {
+            final String normalized = normalizeWord(word);
             if (normalized != null) {
                 inputWords.add(normalized);
             }
@@ -29,6 +37,12 @@ public class WordExtractor {
         return inputWords;
     }
 
+    /**
+     * Builds the list of collation keys for the provided input words using
+     * {@link DiacriticsInsensitiveWord#getCollationKey(String)}
+     * @param inputWords the lowercase words to build the collation key list from
+     * @return the collation keys in order
+     */
     public static List<byte[]> getCollationKeys(final List<String> inputWords) {
         final List<byte[]> inputWordCollationKeys = new ArrayList<>(inputWords.size());
         for (final String inputWord : inputWords) {
@@ -37,11 +51,21 @@ public class WordExtractor {
         return inputWordCollationKeys;
     }
 
+    /**
+     * Extracts a capturing group from the input containing the provided word range. Special
+     * characters before and after the range are kept. The case and diacritics of letters are also
+     * preserved.<br>
+     * For example, extracting [1,3) from "a b, c; d " yields " b, c; " (note how spaces are also
+     * kept).
+     * @param input the original raw input from the user
+     * @param range the range of words representing those captured in the capturing group
+     * @return the content of the capturing group
+     */
     public static String extractCapturingGroup(final String input, final InputWordRange range) {
-        Pattern pattern = Pattern.compile("^(?:[^\\p{L}]*\\p{L}+){" + range.from()
+        final Pattern pattern = Pattern.compile("^(?:[^\\p{L}]*\\p{L}+){" + range.from()
                 + "}((?:[^\\p{L}]*\\p{L}+){" + (range.to() - range.from()) + "}[^\\p{L}]*)");
-        Matcher matcher = pattern.matcher(input);
-        boolean foundMatch = matcher.find();
+        final Matcher matcher = pattern.matcher(input);
+        final boolean foundMatch = matcher.find();
 
         if (foundMatch) {
             return matcher.group(1);
@@ -50,15 +74,15 @@ public class WordExtractor {
         }
     }
 
+    /**
+     * @param word any string
+     * @return the word to lowercase and null if the word is null or empty.
+     */
     private static String normalizeWord(final String word) {
-        try {
-            if(word.isEmpty()) {
-                return null;
-            }
-
-            return word.toLowerCase(Locale.ENGLISH);
-        } catch (Throwable ignored) {
+        if(word == null || word.isEmpty()) {
             return null;
         }
+
+        return word.toLowerCase(Locale.ENGLISH);
     }
 }
