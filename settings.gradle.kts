171e382c5655d424679d54881a3a702b4ebbcba7
Stypox <stypox@pm.me>
2024-05-09 08:26:33 +0200

    Switch to version catalog for plugins and reorder gradle files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = java.net.URI("https://jitpack.io") }
+    }
+}
+
 include(":skill")
 
 // Uncomment these lines to use a local copy of the projects, instead of letting Gradle fetch them
