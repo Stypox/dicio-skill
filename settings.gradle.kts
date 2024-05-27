e4e9d6a57c088c259b89c0107a4abb5d3bb38b03
Stypox <stypox@pm.me>
2024-05-27 19:31:55 +0200

    Get dicio-numbers version from local.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,7 @@
 import me.champeau.gradle.igp.gitRepositories
+import org.eclipse.jgit.api.Git
+import java.io.FileInputStream
+import java.util.Properties
 
 include(":skill")
 
@@ -23,15 +26,47 @@ dependencyResolutionManagement {
     }
 }
 
-gitRepositories {
-    include("dicio-numbers") {
-        uri.set("https://github.com/Stypox/dicio-numbers")
-        tag.set("dummy-3")
-        autoInclude.set(false)
-        includeBuild("") {
-            dependencySubstitution {
-                substitute(module("git.included.build:dicio-numbers"))
-                    .using(project(":numbers"))
+val localProperties = Properties().apply {
+    try {
+        load(FileInputStream(File(rootDir, "local.properties")))
+    } catch (e: Throwable) {
+        println("Warning: can't read local.properties: $e")
+    }
+}
+
+if (localProperties.getOrDefault("useLocalDicioLibraries", "") == "true") {
+    includeBuild("../dicio-numbers") {
+        dependencySubstitution {
+            substitute(module("git.included.build:dicio-numbers"))
+                .using(project(":numbers"))
+        }
+    }
+
+} else {
+    // if the repo has already been cloned, the gitRepositories plugin is buggy and doesn't
+    // fetch the remote repo before trying to checkout the commit (in case the commit has changed),
+    // so we need to do it manually
+    val file = File("$rootDir/checkouts/dicio-numbers")
+    if (file.isDirectory) {
+        Git.open(file).fetch().call()
+    }
+
+    gitRepositories {
+        include("dicio-numbers") {
+            uri.set("https://github.com/Stypox/dicio-numbers")
+            // use the dummy version, except when specified differently in local.properties
+            val overrideCommit = localProperties.getOrDefault("dicioNumbersCommit", "").toString()
+            if (overrideCommit.isBlank()) {
+                tag.set("dummy-3")
+            } else {
+                commit.set(overrideCommit)
+            }
+            autoInclude.set(false)
+            includeBuild("") {
+                dependencySubstitution {
+                    substitute(module("git.included.build:dicio-numbers"))
+                        .using(project(":numbers"))
+                }
             }
         }
     }
